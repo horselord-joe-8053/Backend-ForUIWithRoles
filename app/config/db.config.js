056f2802b5de355c8079a305ec274b932d952f4e
horselord-joe-8053 <coder.joe.8053@gmail.com>
2022-07-11 02:15:10 +1000

    for GKE testing

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  CONNECT_STR: 'localhost:27017',
+  // CONNECT_STR: 'localhost:27017',
   // CONNECT_STR: 'root:secretpassword@34.129.29.32:27017,34.129.233.99:27017',
-  // CONNECT_STR:
-  //   'root:secretpassword@balm1-mongodb-0.balm1-mongodb-headless.default.svc.cluster.local:27017,balm1-mongodb-1.balm1-mongodb-headless.default.svc.cluster.local:27017',
+  CONNECT_STR:
+    'root:secretpassword@balm1-mongodb-0.balm1-mongodb-headless.default.svc.cluster.local:27017,balm1-mongodb-1.balm1-mongodb-headless.default.svc.cluster.local:27017',
   // DB: "bezkoder_db",
   // DB: 'balm-website-DB',
   DB: 'balm-website-DB-test',
