ac6b5af86aa4b732aad5c8b537a8fd04fedc0a64
horselord-joe-8053 <coder.joe.8053@gmail.com>
2022-07-16 00:16:09 +1000

    BSS-137: list of all unpaid dates with checkboxes - backend; unit test with jest

---
+++ app/test/date-utils.test.js
@@ -0,0 +1,118 @@
+const Logger = require('../utils/logger');
+
+const DateUtils = require('../utils/date-utils');
+const { forEach } = require('lodash');
+
+test('DateUtils.getPrevScheduledDateList, FORTNIGHTLY, 2022-03-01 to 2022-04-01', () => {
+  // getScheduledDateListWithIntervalInWeeks(knownPrevDate, weeksPerInterval, anchorDate)
+  let lastKnownPayDateStr = '2022-03-01';
+  let anchorDateStr = '2022-04-01';
+  let frequency = 'FORTNIGHTLY';
+
+  let expectedDateStrList = ['2022-03-01', '2022-03-15', '2022-03-29'];
+
+  let expectedResult = getDateObjList(expectedDateStrList);
+
+  // // unordered
+  // expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+  //   expect.arrayContaining(expectedResult)
+  // );
+
+  // ordered
+  expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+    expectedResult
+  );
+});
+
+test('DateUtils.getPrevScheduledDateList, FORTNIGHTLY, 2022-03-01 to 2022-03-27', () => {
+  // getScheduledDateListWithIntervalInWeeks(knownPrevDate, weeksPerInterval, anchorDate)
+  let lastKnownPayDateStr = '2022-03-01';
+  let anchorDateStr = '2022-03-27';
+  let frequency = 'FORTNIGHTLY';
+
+  let expectedDateStrList = ['2022-03-01', '2022-03-15'];
+
+  let expectedResult = getDateObjList(expectedDateStrList);
+
+  // // unordered
+  // expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+  //   expect.arrayContaining(expectedResult)
+  // );
+
+  // ordered
+  expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+    expectedResult
+  );
+});
+
+test('DateUtils.getPrevScheduledDateList, FORTNIGHTLY, 2022-03-01 to 2022-03-29, edgecase', () => {
+  // getScheduledDateListWithIntervalInWeeks(knownPrevDate, weeksPerInterval, anchorDate)
+  let lastKnownPayDateStr = '2022-03-01';
+  let anchorDateStr = '2022-03-29';
+  let frequency = 'FORTNIGHTLY';
+
+  let expectedResult = getDateObjList(['2022-03-01', '2022-03-15', '2022-03-29']);
+
+  expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+    expectedResult
+  );
+});
+
+// Monthly
+test('DateUtils.getPrevScheduledDateList, MONTHLY, 2021-03-01 to 2021-06-29', () => {
+  // getScheduledDateListWithIntervalInWeeks(knownPrevDate, weeksPerInterval, anchorDate)
+  let lastKnownPayDateStr = '2022-03-01';
+  let anchorDateStr = '2022-06-29';
+  let frequency = 'MONTHLY';
+
+  let expectedDateStrList = ['2022-03-01', '2022-04-01', '2022-05-01', '2022-06-01'];
+  let expectedResult = getDateObjList(expectedDateStrList);
+
+  expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+    expectedResult
+  );
+});
+
+test('DateUtils.getPrevScheduledDateList, MONTHLY, 2022-03-01 to 2022-06-01, edgecase', () => {
+  // getScheduledDateListWithIntervalInWeeks(knownPrevDate, weeksPerInterval, anchorDate)
+  let lastKnownPayDateStr = '2022-03-01';
+  let anchorDateStr = '2022-06-01';
+  let frequency = 'MONTHLY';
+
+  let expectedDateStrList = ['2022-03-01', '2022-04-01', '2022-05-01', '2022-06-01'];
+  let expectedResult = getDateObjList(expectedDateStrList);
+
+  expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+    expectedResult
+  );
+});
+
+test('DateUtils.getPrevScheduledDateList, MONTHLY, 2022-01-31 to 2022-02-28, edgecase', () => {
+  // getScheduledDateListWithIntervalInWeeks(knownPrevDate, weeksPerInterval, anchorDate)
+  let lastKnownPayDateStr = '2022-01-31';
+  let anchorDateStr = '2022-02-28';
+  let frequency = 'MONTHLY';
+
+  let expectedDateStrList = ['2022-01-31', '2022-02-28'];
+  let expectedResult = getDateObjList(expectedDateStrList);
+
+  expect(DateUtils.getPrevScheduledDateList(frequency, lastKnownPayDateStr, anchorDateStr)).toEqual(
+    expectedResult
+  );
+});
+
+function getDateObjList(expectedDateStrList) {
+  // let result = [];
+
+  // forEach() is an 'in order' function
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#description
+  // expectedDateStrList.forEach((str) => {
+  //   result.push(new Date(str));
+  // });
+
+  // map() is an 'in order' function
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#description
+  let result = expectedDateStrList.map((str) => new Date(str));
+
+  return result;
+}
