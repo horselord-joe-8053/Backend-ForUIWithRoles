c41b92081acfdeea4d3d928ee2810223db82e6a6
horselord-joe-8053 <coder.joe.8053@gmail.com>
2022-10-07 17:39:28 +1100

    BSS-183: 'Paid Date List' was not properly sorted

--- app/test/date-utils.test.js
+++ app/test/date-utils.test.js
@@ -3,6 +3,46 @@ const Logger = require('../utils/logger');
 const DateUtils = require('../utils/date-utils');
 const { forEach } = require('lodash');
 
+test('Date Sort', () => {
+  let dateListBeforeSort = [
+    '2022-10-05T00:00:00.000Z',
+    '2022-09-07T00:00:00.000Z',
+    '2022-08-10T00:00:00.000Z',
+    '2022-07-13T00:00:00.000Z',
+    '2022-07-27T00:00:00.000Z',
+    '2022-09-21T00:00:00.000Z',
+  ];
+
+  let expectedAscDateListAfterSort = [
+    '2022-07-13T00:00:00.000Z',
+    '2022-07-27T00:00:00.000Z',
+    '2022-08-10T00:00:00.000Z',
+    '2022-09-07T00:00:00.000Z',
+    '2022-09-21T00:00:00.000Z',
+    '2022-10-05T00:00:00.000Z',
+  ];
+
+  let expectedDescDateListAfterSort = [
+    '2022-10-05T00:00:00.000Z',
+    '2022-09-21T00:00:00.000Z',
+    '2022-09-07T00:00:00.000Z',
+    '2022-08-10T00:00:00.000Z',
+    '2022-07-27T00:00:00.000Z',
+    '2022-07-13T00:00:00.000Z',
+  ];
+
+  dateListBeforeSort.sort(isoDateStrSort);
+
+  expect(dateListBeforeSort).toEqual(expectedAscDateListAfterSort);
+  expect(dateListBeforeSort.reverse()).toEqual(expectedDescDateListAfterSort);
+});
+
+function isoDateStrSort(a, b) {
+  let aDate = new Date(a);
+  let bDate = new Date(b);
+  return aDate < bDate ? -1 : aDate > bDate ? 1 : 0;
+}
+
 test('DateUtils.getPrevScheduledDateList, FORTNIGHTLY, 2022-03-01 to 2022-04-01', () => {
   // getScheduledDateListWithIntervalInWeeks(knownPrevDate, weeksPerInterval, anchorDate)
   let lastKnownPayDateStr = '2022-03-01';
