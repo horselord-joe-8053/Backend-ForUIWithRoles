4e853e647f6635407d6ec0ef6d7fff472c32a4c3
horselord-joe-8053 <coder.joe.8053@gmail.com>
2023-09-22 23:30:41 +1000

    superficial change to trigger CICD

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // CONNECT_STR: 'localhost:27017',
   // CONNECT_STR: 'root:secretpassword@34.129.29.32:27017,34.129.233.99:27017',
-  CONNECT_STR: 'root:secretpassword@34.151.113.116:27017', // GKE
+  CONNECT_STR: 'root:secretpassword@34.151.113.116:27017', // GKE DB
   // CONNECT_STR:
   //   'root:secretpassword@balm1-mongodb-0.balm1-mongodb-headless.default.svc.cluster.local:27017,balm1-mongodb-1.balm1-mongodb-headless.default.svc.cluster.local:27017',
   // DB: "bezkoder_db",
